b48d63a52ca70c3f4069e404cae450a63f89f92f
Vasily Grigoriev <gridark@gmail.com>
2020-11-14 06:31:52 +0300

    feat: skeleton

---
+++ brushwood.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Brushwood
+Description: Is a easy way using the twig accessibility
+Plugin URI: https://upstatement.com/timber
+Author: Vasily Grigoriev
+Version: 1.0.0
+Author URI: http://gridark.ru/
+*/
